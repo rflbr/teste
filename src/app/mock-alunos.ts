01c6d3bf7b270b7091df50e2250ab942f78d6890
Rafael Lebre <32398471+rflbr@users.noreply.github.com>
2018-01-25 21:01:11 -0200

    first commit

---
+++ src/app/mock-alunos.ts
@@ -0,0 +1,12 @@
+import { Aluno } from './aluno';
+
+export const ALUNOS: Aluno [] = [
+  { id: 1, name: 'JoJo' },
+  { id: 2, name: 'Luffy' },
+  { id: 3, name: 'Jonathan'},
+  { id: 4, name: 'Joseph'},
+  { id: 5, name: 'Jotaro'},
+  { id: 6, name: 'Koichi'},
+  { id: 7, name: 'Josuke'},
+
+]
